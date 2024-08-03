2509ddb2afc9cb506de9f7e3266e5c39f1d47d07
malreddy <root@localhost.localdomain>
2024-08-03 20:39:58 +0530

    1st master code commit

---
+++ script2.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+if [ -x netstat ]
+then  
+	echo this file is has execute permission available
+else
+	echo this file is has not execute permisssion available
+fi
+
---
+++ scrit1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+if [ -x /proc/cpuinfo ]
+then 
+	echo this file has execute permission
+else 
+	echo this file has not execute permission
+fi
+
